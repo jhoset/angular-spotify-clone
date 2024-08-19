ef0465c79a6d2ab6460b94dcfb789906c2d47d7c
Jhoset NG <adm_dng@hotmail.com>
2024-08-18 23:45:32 -0500

    Improve AsideMenuComponent - Removed unnecessary code to fetch data - consume common global service

--- src/app/shared/aside-menu/aside-menu.component.ts
+++ src/app/shared/aside-menu/aside-menu.component.ts
@@ -1,10 +1,9 @@
-import {Component, signal} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
 import {SvgIconComponent} from "angular-svg-icon";
 import {SideMenuItemComponent} from "@shared/aside-menu/components/side-menu-item/side-menu-item.component";
 import {SideMenuCardComponent} from "@shared/aside-menu/components/side-menu-card/side-menu-card.component";
-import {SpotifyPlaylist} from "@core/services/spotify/interfaces/spotify";
-import {SpotifyService} from "@core/services/spotify/spotify.service";
+import {CommonDashboardService} from "../../dashboard/common-dashboard.service";
 
 @Component({
   selector: 'app-aside-menu',
@@ -19,15 +18,5 @@ import {SpotifyService} from "@core/services/spotify/spotify.service";
   styleUrl: './aside-menu.component.scss'
 })
 export class AsideMenuComponent {
-
-  public spotifyPlayLists = signal<SpotifyPlaylist[]>([])
-
-
-  constructor(private spotifyService: SpotifyService) {
-    this.spotifyService.getCurrentUserPlaylists().subscribe(rs => {
-      console.log(rs.items)
-      this.spotifyPlayLists.set(rs.items);
-    })
-  }
-
+  public commonService = inject(CommonDashboardService);
 }
