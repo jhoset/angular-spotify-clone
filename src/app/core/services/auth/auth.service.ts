7e2a437c055ec1e309173319eceef513413ecdea
Jhoset NG <adm_dng@hotmail.com>
2024-09-01 20:21:07 -0500

    Fix refresh token issue

--- src/app/core/services/auth/auth.service.ts
+++ src/app/core/services/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import {inject, Injectable} from '@angular/core';
 import {HttpClient, HttpParams} from "@angular/common/http";
+import {environment} from "../../../../environments/environment.development";
 
 export interface AuthRefreshTokenResponse {
   accessToken: string;
@@ -16,6 +17,6 @@ export class AuthService {
   refreshToken(refreshToken: string) {
     let params = new HttpParams();
     params = params.set('refresh_token', refreshToken);
-    return this.http.get<AuthRefreshTokenResponse>('http://localhost:3000/api/auth/refresh_token', {params});
+    return this.http.get<AuthRefreshTokenResponse>(`${environment.SERVER_URL}/api/auth/refresh_token`, {params});
   }
 }
